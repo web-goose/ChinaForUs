efa9e974db48548f20605c222011a3d85ead3064
TrimokRu <47853066+TrimokRu@users.noreply.github.com>
2019-10-10 14:36:50 +0300

    add activity

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,43 @@ wow.init();
 function scrollToAnchor(anchorName) {
     $('html, body').stop().animate({scrollTop: $(anchorName).offset().top}, 1000);
 }
+
+const text = "время и деньги";
+let writerCount = 0;
+let typeWriterInterval;
+let typeCleanerInterval;
+function typeWriter() {
+    typeWriterInterval = setInterval(function () {
+        if(writerCount >= text.length) {
+            console.log(writerCount + '|' + text.length)
+            setTimeout(function () {
+                typeCleaner();
+            }, 2000);
+            clearInterval(typeWriterInterval);
+            return;
+        }
+        $('.typewriter').text($('.typewriter').text() + text[writerCount]);
+        writerCount++;
+    },100);
+}
+
+function typeCleaner() {
+    clearInterval(typeWriterInterval);
+    typeCleanerInterval = setInterval(function () {
+        if(writerCount <= 0) {
+            setTimeout(function () {
+                console.log(writerCount + '| cleaner')
+                typeWriter();
+            }, 2000);
+            clearInterval(typeCleanerInterval);
+            return;
+        }
+
+        $('.typewriter').text($('.typewriter').text().slice(0, -1));
+        writerCount--;
+    },100);
+}
+
+(function () {
+    typeWriter();
+}());
